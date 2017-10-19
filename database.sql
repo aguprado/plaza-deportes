db1b22620cacf7ae55f710bbdb437dc3dbd3c604
Agustin Prado <aguprado@gmail.com>
2017-10-19 11:04:36 -0300

    avances

--- database.sql
+++ database.sql
@@ -1,8 +1,9 @@
 CREATE TABLE grupo (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  nombre varchar(256) NOT NULL DEFAULT '40bd001563085fc35165329ea1ff5c5ecbdbbeef',
-  descripcion varchar(256) NOT NULL DEFAULT '40bd001563085fc35165329ea1ff5c5ecbdbbeef',
+  nombre varchar(256) NOT NULL,
+  descripcion varchar(256) NOT NULL,
   dias varchar(256) NOT NULL,
   horarios varchar(256) NOT NULL,
+  cupos int(4) NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
